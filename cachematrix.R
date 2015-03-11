d74a6ab759a27711ae4dfe8728e3c8d746c6d340
Michael Wenocur <mlwenocur@users.noreply.github.com>
2015-03-10 23:45:20 -0700

    Completed stub functions to implement required behavior.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list of functions and with an enclosing
+## environment, returning the list of functions. Its behavior looks a lot
+## a constructor for an object whose member are a matrix x and 
+## its inverse matInv, and whose methods are getMat, setMat, getInv and setInv, 
+## corresponding to methods for retrieving and setting x and 
+## for retrieving and setting matInv.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  matInv <- NULL
+  setMat <- function(nextMat) {
+    x <<- nextMat;
+    matInv <<- NULL
+  }
+  
+  getMat <- function()x
+  
+  setInv <- function(matI){
+    matInv <<- matI
+  }
+  
+  getInv <- function(){
+    matInv
+  }
+  list(setMat = setMat, getMat = getMat, setInv = setInv, getInv = getInv)
+  
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes as its argument the output from makeCacheMatrix and returns
+## the cached inverse if available, and other computes and caches the inverse,
+## and returns the newly generated inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <-x$getInv()
+  if (!is.null(inv)) {
+    message("returning cached inv") #Included for ease of testing caching feature
+    return(inv)
+  }
+  
+  mat <- x$getMat()
+  inv <- solve(mat)
+  x$setInv(inv)
+  inv
 }
